7d071d01972c0ca3bd2262f073da9b1379f4fea7
Aleksandr19991 <am79250198173@yandex.ru>
2025-01-19 18:44:56 +0300

    Create HttpClient

---
+++ RatesProvider.Application/Integrations/FixerClient.cs
@@ -0,0 +1,33 @@
+﻿using RatesProvider.Application.Models.CurrencyApiModels;
+using System.Text.Json;
+
+namespace RatesProvider.Application.Integrations
+{
+    public class FixerClient
+    {
+        private readonly HttpClient _client;
+        private readonly JsonSerializerOptions _options;
+
+        public FixerClient(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri("https://fixer.io/");
+            _client.Timeout = new TimeSpan(0, 0, 30);
+            _client.DefaultRequestHeaders.Clear();
+
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<List<CurrencyResponse>> GetCurrencyRatesAsync()
+        {
+            using (var response = await _client.GetAsync("FixerRates", HttpCompletionOption.ResponseHeadersRead))
+            {
+                response.EnsureSuccessStatusCode();
+                var stream = await response.Content.ReadAsStreamAsync();
+                var currency = await JsonSerializer.DeserializeAsync<List<CurrencyResponse>>(stream, _options);
+                return currency;
+            }
+        }
+
+    }
+}
