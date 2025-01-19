3b1a610497716bc4d5fee7f49bf35660fd7d36a5
Aleksandr19991 <am79250198173@yandex.ru>
2025-01-19 18:57:43 +0300

    Setting up dependencies

--- RatesProvider/Program.cs
+++ RatesProvider/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 using RatesProvider.Application.Configuration;
+using RatesProvider.Application.Integrations;
 
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -20,13 +21,17 @@ builder.Configuration
 
 var configuration = builder.Configuration;
 
-
 LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
-
+builder.Services.AddHostedService<Worker>();
 builder.Logging.AddConfiguration(
     builder.Configuration.GetSection("Logging"));
 
+
+builder.Services.AddHttpClient<CurrencyApiClient>();
+builder.Services.AddHttpClient<CurrencyLayerClient>();
+builder.Services.AddHttpClient<FixerClient>();
 builder.Services.AddApplicationServices();
+
 
 var host = builder.Build(); 
 host.Run();
--- RatesProvider/Worker.cs
+++ RatesProvider/Worker.cs
@@ -1,32 +1,25 @@
-﻿using RatesProvider.Application.Services;
+﻿using RatesProvider.Application.Interfaces;
 
 
 public class Worker : BackgroundService
-
 {
     private readonly ILogger<Worker> _logger;
-    private readonly CurrencyProvider1 _currencyProvider;
+    private readonly ICurrencyApiService _currencyProviderService;
     private readonly string _baseCurrency = "USD"; //  базовую валюту
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddSingleton<CurrencyProvider1>(); // или другой подходящий жизненный цикл
-        services.AddHostedService<Worker>();
-    }
-
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, ICurrencyApiService currencyProviderService)
     {
         _logger = logger;
-        _currencyProvider = new CurrencyProvider1("cur_live_IiK9hxawHQROFiX3rUYiEMzPzQ6oa12EmOqDrSiN");
+        _currencyProviderService = currencyProviderService;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-      while(!stoppingToken.IsCancellationRequested)
-      {
+        while (!stoppingToken.IsCancellationRequested)
+        {
             _logger.LogInformation("RatesProviderService running at: {time}", DateTimeOffset.Now);
-            await _currencyProvider.GetRatesAsync(_baseCurrency);
+            await _currencyProviderService.GetCurrencyRateWithTypedClientAsync();
             await Task.Delay(TimeSpan.FromMinutes(1));
-      }
+        }
     }
 }
