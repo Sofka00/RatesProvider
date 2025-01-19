6b4769730d5c36dbc583b33c87a6a822f7e327b0
Aleksandr19991 <am79250198173@yandex.ru>
2025-01-19 18:54:52 +0300

    Refresh and Delete CurrencyApiService

---
+++ RatesProvider.Application/Services/CurrencyApiService.cs
@@ -0,0 +1,23 @@
+﻿using RatesProvider.Application.Integrations;
+using RatesProvider.Application.Interfaces;
+
+
+
+namespace RatesProvider.Application.Services;
+
+public class CurrencyApiService : ICurrencyApiService
+{
+    private readonly CurrencyApiClient _currencyProviderClient;
+
+    public CurrencyApiService(CurrencyApiClient currencyProviderClient)
+    {
+        _currencyProviderClient = currencyProviderClient;
+    }
+
+    public async Task Execute()
+    {
+        await GetCurrencyRateWithTypedClientAsync();
+    }
+
+    public async Task GetCurrencyRateWithTypedClientAsync() => await _currencyProviderClient.GetCurrencyRatesAsync();
+}
