7e9405219290437765190b0b41cb40c4b7ca00aa
Aleksandr19991 <am79250198173@yandex.ru>
2025-01-26 21:56:37 +0300

    Create CurrencyRateManager

---
+++ RatesProvider.Application/Services/CurrencyRateManager.cs
@@ -0,0 +1,20 @@
+﻿using RatesProvider.Application.Interfaces;
+using RatesProvider.Application.Models;
+
+namespace RatesProvider.Application.Services;
+
+public class CurrencyRateManager
+{
+    private readonly IRatesProviderContext _ratesProviderContext;
+
+    public CurrencyRateManager(IRatesProviderContext ratesProviderContext)
+    {
+        _ratesProviderContext = ratesProviderContext;
+    }
+
+    public async Task<CurrencyRate> GetRate()
+    {
+
+        return null;
+    }
+}
