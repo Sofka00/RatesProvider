fe698058dfecf84447c5ed6be3e2f9c6f1ecd7f2
Sofiya <pitenko.sofiya@mail.ru>
2025-02-09 15:11:55 +0300

    Add loggin in Manager and  Context

--- RatesProvider.Application/Services/CurrencyRateManager.cs
+++ RatesProvider.Application/Services/CurrencyRateManager.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using RatesProvider.Application.Interfaces;
 using RatesProvider.Application.Models;
 
@@ -8,21 +9,44 @@ namespace RatesProvider.Application.Services
     {
         private readonly ICurrencyRateProvider _providerFixer;
         private IRatesProviderContext _context;
+        ILogger<CurrencyRateManager> _logger;
 
-        public CurrencyRateManager(IRatesProviderContext context, [FromKeyedServices("Fixer")] ICurrencyRateProvider providerFixer)
+        public CurrencyRateManager(IRatesProviderContext context, [FromKeyedServices("Fixer")] ICurrencyRateProvider providerFixer, ILogger<CurrencyRateManager> logger)
         {
             _context = context;
             _providerFixer = providerFixer;
+            _logger = logger;
 
+            _logger.LogInformation("CurrencyRateManager initialized with provider: {ProviderType}", _providerFixer.GetType().Name);
         }
 
         public async Task<CurrencyRateResponse> GetRatesAsync()
         {
             CurrencyRateResponse result = default;
 
-            _context.SetCurrencyRatesProvider(_providerFixer);
+            try
+            {
+                _logger.LogInformation("Setting the currency rate provider to: {ProviderType}", _providerFixer.GetType().Name);
+                _context.SetCurrencyRatesProvider(_providerFixer);
 
-            result = await _context.GetRatesAsync();
+                _logger.LogInformation("Attempting to retrieve currency rates...");
+
+                result = await _context.GetRatesAsync();
+
+                if (result != null)
+                {
+                    _logger.LogInformation("Successfully retrieved currency rates.");
+                }
+                else
+                {
+                    _logger.LogWarning("No currency rates were returned.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "An error occurred while retrieving currency rates.");
+            }
 
             return result;
         }
